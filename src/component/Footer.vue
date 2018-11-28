58fb62287a3ac7c34d5a09e5af9f6f51adb97dec
Jireh <941d05@gmail.com>
2018-11-28 21:55:54 +0900

    Footer.vue 마크업 코드 추가

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -1,4 +1,14 @@
-<template></template>
+<template>
+    <footer>
+        <div class="container">
+            <a href="/" class="logo-font">conduit</a>
+            <span class="attribution">
+                An interactive learning project from
+                <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+            </span>
+        </div>
+    </footer>
+</template>
 
 <script>
 export default {

0dcbe00acd8366240ad9e8e746b835d1ade8fa33
Jireh <941d05@gmail.com>
2018-11-28 22:13:40 +0900

    Header, Footer component 등록

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
     <div id="app">
+        <Header></Header>
         <router-view></router-view>
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        Header,
+        Footer
+    }
 };
 </script>

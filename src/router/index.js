438049ac872f23eb8099a6ebbd2e7e8b78401d99
Jireh <941d05@gmail.com>
2018-12-07 16:25:17 +0900

    login routes추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "../component/Home";
 import Register from "../component/Register";
+import Login from "../component/Login";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
             name: "register",
             path: "/register",
             component: Register
+        },
+        {
+            name: "login",
+            path: "/login",
+            component: Login
         }
     ]
 });
